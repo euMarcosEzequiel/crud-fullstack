a8b89f5564dc6e05ba4c90c03bfb5c7a9ff2dbee
Marcos Marques <marcos.marques@mosb.local>
2024-10-17 10:17:22 -0300

    Adding messages
    Adding feedback messages to the user

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { Sidebar } from "@/components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <div className="w-screen h-screen bg-neutral-900 flex box-border">
+    <div>
       <Sidebar />
       <main className="flex-1 h-full p-4">
         <Component {...pageProps} />
+        <ToastContainer 
+          autoClose={2500} 
+          position={"top-right"} 
+        />
       </main>
     </div>
   );
---
+++ frontend/src/services/UserServices.ts
@@ -0,0 +1,46 @@
+import { api } from "@/api/api";
+import { toast } from "react-toastify";
+
+class UserServices{
+    async getUsers(){
+        try {
+            const response = await api.get("/users");
+            return response.data;
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Error finding all users!");
+        }
+    }
+
+    async create(name: string, email: string){
+        try {
+            const response = await api.post("/user", {
+                name: name,
+                email: email,
+            });
+            toast.success(response.data.message);
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Error creating user!");
+        }
+    }
+
+    async delete(id: string){
+        try {
+            const response = await api.delete("/user", {
+                params:{
+                    id: id,
+                }
+            });
+            toast.success(response.data.message);
+            
+        } catch (error) {
+            console.log(error);
+            toast.error("Error deleting user!");
+        }
+    }
+}
+
+export { UserServices };
